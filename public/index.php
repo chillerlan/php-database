9f6797177471a38091a32a46bfe152675b28cb3e
smiley <smiley@chillerlan.net>
2017-07-09 01:33:22 +0200

    :sparkles:

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @filesource   index.php
+ * @created      09.07.2017
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2017 Smiley
+ * @license      MIT
+ */
+
+
+phpinfo();
+
+
